ed3c37fb4fed380ce078cbfd3f6a599e830240c7
sophie mattacks <s.mattacks@hotmail.co.uk>
2022-11-22 11:56:29 +0000

    Sign Up page validations complete, created helper for validateAllFormFields()

--- src/app/components/signup/signup.component.ts
+++ src/app/components/signup/signup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import ValidateForm from 'src/app/helpers/validateForm';
 
 @Component({
   selector: 'app-signup',
@@ -10,10 +12,18 @@ export class SignupComponent implements OnInit {
   type: string = "password";
   isText: boolean = false;
   eyeIcon: string = "fa-eye-slash";
+  signupForm!: FormGroup;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupForm = this.fb.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    })
   }
 
   hideShowPass(){
@@ -21,5 +31,15 @@ export class SignupComponent implements OnInit {
     this.isText ? this.eyeIcon = "fa-eye" : this.eyeIcon = "fa-eye-slash";
     this.isText ? this.type = "text" : this.type = "password";
   }
+
+  onSignup(){
+    if(this.signupForm.valid){
+      console.log(this.signupForm.value)
+
+    }else{
+      ValidateForm.validateAllFormFields(this.signupForm)
+    }
+  }
+
 
 }
